d2d812f6508d940365c49b30f6a85f261a964d7e
tunnckoCore <mameto_100@mail.bg>
2015-04-22 12:35:50 +0300

    update tests

--- test/index.js
+++ test/index.js
@@ -14,13 +14,13 @@ var transform = require('../index');
 test('should transform modern CSS with topcoat-resin', function(done) {
   var fixture = fs.readFileSync('./test/fixture.css', 'utf8');
   var expected = fs.readFileSync('./test/expected.css', 'utf8');
-  var actual = transform.render(':root { var-grey: #666; } .btn { box-shadow: 0 1px var(grey); }', {
+  var actual = transform.render(fixture, {
     namespace: 'topcoat',
-    // license: fs.readFileSync('./test/license.txt', 'utf8'),
+    license: fs.readFileSync('./test/license.txt', 'utf8'),
     url: 'img/'
   });
-  // console.log(actual)
-  test.equal(actual, '.topcoat-btn {\n  box-shadow: 0 1px #666\n}');
+
+  test.equal(actual, expected);
   done();
 });
 
